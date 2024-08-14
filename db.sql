c8d8ec9a3350ecff1bd8ba9514e422a78ba5dcc3
Jayesh Bhoot <jysh@jysh.net>
2024-08-14 19:40:59 +0530

    Reduce db script to a minimum

--- db.sql
+++ db.sql
@@ -4,10 +4,10 @@ create table testpoint(pos point NOT NULL) ENGINE=InnoDB;
 
 insert into testpoint values (st_geomfromtext('POINT(-33.3109317 117.3461733)', 4326));
 
+insert into testpoint values (st_geomfromtext('POINT(-35.3109317 101.3461733)', 0));
+
 -- Following INSERT FAILS in mysql8 because lat-long validation is enforced for SRID 4326.
 -- insert into testpoint values (st_geomfromtext('POINT(100 -33)', 4326));
 
-insert into testpoint values (st_geomfromtext('POINT(-35.3109317 101.3461733)', 0));
-
-insert into testpoint values (st_geomfromtext('POINT(101 -35)', 0));
-
+-- Remove extra records
+-- insert into testpoint values (st_geomfromtext('POINT(101 -35)', 0));
