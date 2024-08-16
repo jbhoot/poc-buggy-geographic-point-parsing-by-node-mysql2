77ddca5cb98581a7fdec9078a8dd505d15f30479
Jayesh Bhoot <jysh@jysh.net>
2024-08-16 22:11:28 +0530

    consolidate query and output

--- db.sql
+++ db.sql
@@ -4,6 +4,8 @@ create table testpoint(pos point NOT NULL) ENGINE=InnoDB;
 
 insert into testpoint values (st_geomfromtext('POINT(-33.3109317 117.3461733)', 4326));
 
+insert into testpoint values (st_geomfromtext('POINT(120 -40)', 4326, 'axis-order=long-lat'));
+
 insert into testpoint values (st_geomfromtext('POINT(-35.3109317 101.3461733)', 0));
 
 -- Following INSERT FAILS in mysql8 because lat-long validation is enforced for SRID 4326.
--- index.js
+++ index.js
@@ -2,19 +2,7 @@ import 'dotenv/config';
 import mysql from 'mysql2/promise';
 
 const queryXYAsConvertedByMySql2Driver = async conn => {
-    const sql = 'SELECT pos FROM testpoint';
-    const [results, _fields] = await conn.execute(sql);
-    return results;
-}
-
-const queryXYAsInterpretedByMySql = async conn => {
-    const sql = 'SELECT st_x(pos), st_y(pos), st_srid(pos) FROM testpoint';
-    const [results, _fields] = await conn.execute(sql);
-    return results;
-}
-
-const queryLatLongAsInterpretedByMySql = async conn => {
-    const sql = 'SELECT st_latitude(pos), st_longitude(pos) FROM testpoint';
+    const sql = "SELECT pos, st_x(pos) as x, st_y(pos) as y, st_srid(pos) as srid, ST_GeomFromWKB(ST_AsWKB(pos)) as pos2 FROM testpoint where st_srid(pos) = 4326";
     const [results, _fields] = await conn.execute(sql);
     return results;
 }
@@ -36,17 +24,18 @@ const main = async () => {
 
     console.log(`Testing with MySQL v${process.env.MYSQL_VERSION}...`, '\n');
 
-    console.log("AS INTERPRETED BY MYSQL8's ST_X, ST_Y functions:")
-    const xy = await queryXYAsInterpretedByMySql(conn);
-    console.table(xy);
-
-    console.log("AS INTERPRETED BY node-mysql2:")
     const results = await queryXYAsConvertedByMySql2Driver(conn);
+
+    console.log("AS READ DIRECTLY BY node-mysql2:")
     console.table(results.map(r => r.pos));
 
+    console.log("AS ST_X, ST_Y functions:")
+    console.table(results.map(r => ({x: r.x, y: r.y, srid: r.srid})));
+
+    console.log("AS wkb->geom:")
+    console.table(results.map(r => r.pos2));
+
     if (process.env.MYSQL_VERSION === '8') {
-        // const latLong = await queryLatLongAsInterpretedByMySql(conn);
-        // console.log(latLong);
         console.log('NOTE: st_latitude() and st_longitude() version omitted because some records contain Point with SRID 0, i.e., not 4326.');
     } else {
         console.log('NOTE: st_latitude() and st_longitude() version omitted because these functions do not exist in v5.');
